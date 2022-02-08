0ea902be23c055957e4ddf8efb202fec1218a2c2
nikit <nikit@DESKTOP-394UBOO>
2022-02-08 15:21:46 +0300

    Добавьте файлы проекта.

---
+++ PartClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ProjectLearn
+{
+    public partial class Service
+    {
+        
+        public SolidColorBrush DisColor
+        {
+            get
+            {
+                    switch (Discount.Value !=0)
+                    {
+                    case true:
+                        return Brushes.LightGreen;
+                    default:
+                        return Brushes.White;
+                    }
+                
+               
+            }
+     
+        }
+    }
+}
